377110dc73717ef4d9eaae00b217cabca7ab6fd0
johann.paez <johann.paez@mail.escuelaing.edu.co>
2020-03-01 20:40:57 -0500

    Fronend Views

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,9 +1,52 @@
 var app = (function () {
 	var nombreAutor = "";		
-	var planos = [];
+	var planos = [];	
+	var callBack = function(error, datos) {
+		if (error != null) {
+			return;
+		}
+		planos = datos.map(function(blueprint) {
+			return {
+				name: blueprint.name,
+				puntos: blueprint.points.length
+			}
+		});
+		var stringTable = table();
+		$("#idTable").html(stringTable);
+		var getSum = planos.reduce(sumTotalPoints, 0);
+		$("#idGetSum").text("Total user points: " + getSum);
+	}
+	var table = function() {
+		var tabla = "<table class='table' style = 'width:500px; align-content:center;'>" +
+						"<thead class='thead-dark'>" +
+							"<tr>" +
+								"<th scope='col'> Blueprint name </th>" +
+								"<th scope='col'> Number of points </th>" +
+								"<th scope='col'> </th>" +
+							"</tr>" +
+						"</thead>" +
+						"<tbody>";
+		planos.forEach(function(plano) {
+			tabla += "<tr>" +
+						"<td>" + plano.name + "</td>" +
+						"<td>" + plano.puntos + "</td>" +
+						"<td> <button type='button' class='btn btn-secondary btn-sm'> Open </button> </td>" +
+					"</tr>";
+		});
+		tabla += "</tbody> </table>";
+		return tabla;
+	}
+	
+	var sumTotalPoints = function(total, blueprint) {
+		return total + blueprint.puntos;
+	}
+	
 	return {
         setNameAuthor: function(author) {
             nombreAutor = author;
-        }
-    }
+        },        
+        updatePlanes: function(author) {
+        	var Blueprint = apimock.getBlueprintsByAuthor(author, callBack); 
+        }        
+    }	
 })();
